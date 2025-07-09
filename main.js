daaa8088d08122d3d8b87fb9563e78be26a71edf
AnLaxina <anilovlaxina@gmail.com>
2025-07-09 14:35:05 -0700

    Add basic implementation of constructing a binary tree

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import Tree from './tree.js';
 
-const coolTree = new Tree();
+const coolArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const coolTree = new Tree(coolArray);
 console.log(coolTree.root);
--- tree.js
+++ tree.js
@@ -3,10 +3,23 @@ import Node from './node.js';
 export default class Tree {
 
     constructor(array) {
-        this.root = this.#buildTree();
+        this.root = this.#buildTree(array);
     }
 
     #buildTree(array) {
-        return "chicken";
+        return this.#buildTreeHelper(array, 0, array.length - 1);
+    }
+
+    #buildTreeHelper(array, startIndex, endIndex) {
+        if(startIndex > endIndex) {
+            return null;
+        }
+
+        let middleIndex = (startIndex + endIndex) / 2;
+        const rootNode = new Node(array[middleIndex]);
+        rootNode.left = this.#buildTreeHelper(array, 0, middleIndex - 1);
+        rootNode.right = this.#buildTreeHelper(array, middleIndex + 1, endIndex);
+
+        return rootNode;
     }
 }
