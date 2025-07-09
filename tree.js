b135ca1bd99fadc3e59458ef3de1f4494c85caf2
AnLaxina <anilovlaxina@gmail.com>
2025-07-09 14:37:51 -0700

    Add fixes to constructing the binary tree

--- tree.js
+++ tree.js
@@ -15,9 +15,11 @@ export default class Tree {
             return null;
         }
 
-        let middleIndex = (startIndex + endIndex) / 2;
+        // Use Math.floor to ensure that the index does not become a decimal value
+        // By default, division in JS rounds it to 1 decimal point including decimals
+        let middleIndex = Math.floor((startIndex + endIndex) / 2);
         const rootNode = new Node(array[middleIndex]);
-        rootNode.left = this.#buildTreeHelper(array, 0, middleIndex - 1);
+        rootNode.left = this.#buildTreeHelper(array, startIndex, middleIndex - 1);
         rootNode.right = this.#buildTreeHelper(array, middleIndex + 1, endIndex);
 
         return rootNode;
