da6a3666d9150fe96512acdaf0ca8ec1a2020f94
AnLaxina <anilovlaxina@gmail.com>
2025-07-14 09:25:40 -0700

    Add working deleteItem() method

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-import Tree from './tree.js';
+import Tree from "./tree.js";
 
 const coolArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const coolTree = new Tree(coolArray);
-console.log('Here is the initial tree before insert: ');
+console.log("Here is the initial tree before insert: ");
 coolTree.prettyPrint();
-console.log('Here it is after insert:');
+console.log("Here it is after insert:");
 coolTree.insert(20);
+coolTree.prettyPrint();
+
+console.log("Now, let's delete 20:");
+coolTree.deleteItem(20);
 coolTree.prettyPrint();
--- tree.js
+++ tree.js
@@ -1,92 +1,109 @@
-import Node from './node.js';
+import Node from "./node.js";
 
 export default class Tree {
+  constructor(array) {
+    this.root = this.#buildTree(array);
+  }
 
-    constructor(array) {
-        this.root = this.#buildTree(array);
+  insert(newValue) {
+    this.root = this.#insertRecursively(this.root, newValue);
+  }
+
+  deleteItem(valueToDelete) {
+    this.root = this.#deleteRecursively(this.root, valueToDelete);
+  }
+
+  prettyPrint(node = this.root, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? "│   " : "    "}`,
+        false
+      );
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  }
+
+  #buildTree(array) {
+    return this.#buildTreeHelper(array, 0, array.length - 1);
+  }
+
+  #buildTreeHelper(array, startIndex, endIndex) {
+    if (startIndex > endIndex) {
+      return null;
     }
 
-    insert(newValue) {
-        this.root = this.#insertRecursively(this.root, newValue);
+    // Use Math.floor to ensure that the index does not become a decimal value
+    // By default, division in JS rounds it to 1 decimal point including decimals
+    let middleIndex = Math.floor((startIndex + endIndex) / 2);
+    const rootNode = new Node(array[middleIndex]);
+    rootNode.left = this.#buildTreeHelper(array, startIndex, middleIndex - 1);
+    rootNode.right = this.#buildTreeHelper(array, middleIndex + 1, endIndex);
+
+    return rootNode;
+  }
+
+  #insertRecursively(root, newValue) {
+    if (root === null) {
+      return new Node(newValue);
+    }
+    // Cannot insert duplicate values
+    if (newValue === root.value) {
+      return root;
     }
 
-    prettyPrint(node = this.root, prefix = '', isLeft = true) {
-        if (node === null) {
-            return;
-        }
-        if (node.right !== null) {
-            this.prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-        }
-        console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
-        if (node.left !== null) {
-            this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-        }
+    if (newValue < root.value) {
+      root.left = this.#insertRecursively(root.left, newValue);
+    } else {
+      root.right = this.#insertRecursively(root.right, newValue);
     }
 
-    #buildTree(array) {
-        return this.#buildTreeHelper(array, 0, array.length - 1);
+    return root;
+  }
+
+  #deleteRecursively(root, valueToDelete) {
+    if (root === null) {
+      return root;
     }
 
-    #buildTreeHelper(array, startIndex, endIndex) {
-        if (startIndex > endIndex) {
-            return null;
-        }
+    // Keep traversing either side of the tree until `valueToDelete` is found in the tree
+    if (valueToDelete < root.value) {
+      root.left = this.#deleteRecursively(root.left, valueToDelete);
+    } else if (valueToDelete > root.value) {
+      root.right = this.#deleteRecursively(root.right, valueToDelete);
+    } else {
+      // If there is a match, consider the edge cases
 
-        // Use Math.floor to ensure that the index does not become a decimal value
-        // By default, division in JS rounds it to 1 decimal point including decimals
-        let middleIndex = Math.floor((startIndex + endIndex) / 2);
-        const rootNode = new Node(array[middleIndex]);
-        rootNode.left = this.#buildTreeHelper(array, startIndex, middleIndex - 1);
-        rootNode.right = this.#buildTreeHelper(array, middleIndex + 1, endIndex);
+      // Case 1 & 2: Node has at least 1 child
+      if (root.left === null) {
+        return root.right;
+      } else if (root.right === null) {
+        return root.left;
+      }
 
-        return rootNode;
+      // Case 3: If both children are present find the "successor" or the min value in the right subtree
+      let successor = this.#minValueNode(root.right);
+      root.value = successor.value;
+
+      // Then, delete the in-order successor
+      root.right = this.#deleteRecursively(root.right, successor.value);
     }
 
-    #insertRecursively(root, newValue) {
-        if (root === null) {
-            return new Node(newValue);
-        }
-        // Cannot insert duplicate values
-        if (newValue === root.value) {
-            return root;
-        }
+    return root;
+  }
 
-        if (newValue < root.value) {
-            root.left = this.#insertRecursively(root.left, newValue);
-        } else {
-            root.right = this.#insertRecursively(root.right, newValue);
-        }
-
-        return root;
+  #minValueNode(node) {
+    let current = node;
+    while (current.left !== null) {
+      current = current.left;
     }
 
-    #deleteRecursively(root, valueToDelete) {
-        if (root === null) {
-            return root;
-        }
-
-        // Keep traversing either side of the tree until `valueToDelete` is found in the tree
-        if(valueToDelete < root.value) {
-            root.left = this.#deleteRecursively(root.left, valueToDelete);
-        }
-        else if(valueToDelete > root.value) {
-            root.right = this.#deleteRecursively(root.right, valueToDelete);
-        }
-        else {
-            // If there is a match, consider the edge cases
-
-            // 1. Root has 0 children or only right child
-            if(root.left === null) {
-                return root.right;
-            }
-
-            // 2. Root only has a left child
-            if(root.right === null) {
-                return root.left;
-            }
-
-            // 3. If both children are present
-            
-        }
-    }
+    return current;
+  }
 }
